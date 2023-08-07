a5cd2b7b3baad90c2364ca49250970d4334e6b68
JeffreyTheCreator <JeffreyEjeanye@gmail.com>
2023-08-07 19:58:28 +0200

    commit

---
+++ src/main/java/de/jeezyhub/Main.java
@@ -0,0 +1,24 @@
+package de.jeezyhub;
+
+
+import de.jeezyhub.colors.Color;
+import de.jeezyhub.events.JoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println(Color.WHITE_BOLD+"[JeezyDevelopment]"+Color.GREEN_BOLD+" Successfully"+Color.CYAN_BOLD+" started JeezyHub coded by JeezyDevelopment!"+Color.RESET);
+        EventsRegister();
+    }
+
+    private void EventsRegister() {
+        getServer().getPluginManager().registerEvents(new JoinEvent(), this);
+    }
+
+    @Override
+    public void onDisable() {
+        System.out.println(Color.WHITE_BOLD+"[JeezyHub]"+Color.RED_BOLD+" shutting down..."+Color.RESET);
+    }
+}
---
+++ src/main/java/de/jeezyhub/events/JoinEvent.java
@@ -0,0 +1,26 @@
+package de.jeezyhub.events;
+
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class JoinEvent implements Listener {
+
+    @EventHandler
+    public void onJoinEvent(PlayerJoinEvent e) {
+        e.getPlayer().sendMessage(new String[] {
+                "\n",
+                " §9§lMineral Network §7§l(§6§lSeason §f§l1§7§l)\n",
+                "\n",
+                " §9§l♦ §f§lWebsite: §9§lmineral.gg\n",
+                "\n",
+                " §9§l♦ §f§lStore: §9§lstore.mineral.gg\n",
+                "\n",
+                " §9§l♦ §f§lDiscord: §9§ldiscord.mineral.gg\n",
+                "\n",
+                " §9§l♦ §f§lTwitter: §9§ltwitter.com/MineralServer\n",
+                "\n"
+        });
+    }
+}
